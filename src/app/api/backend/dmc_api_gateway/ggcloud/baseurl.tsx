85745e61dd1d0fd78a1cbca9a5aeb6461645fb55
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2025-07-31 09:49:29 +0700

    Update baseurl.tsx

--- src/app/api/backend/dmc_api_gateway/ggcloud/baseurl.tsx
+++ src/app/api/backend/dmc_api_gateway/ggcloud/baseurl.tsx
@@ -1,2 +1,3 @@
-const BASEURL = "https://datn-08-2025-back-end-360694025581.asia-east1.run.app";
+const BASEURL = "";
+const HIDDEN = "https://datn-08-2025-back-end-360694025581.asia-east1.run.app";
 export default BASEURL;
